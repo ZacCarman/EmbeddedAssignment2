ca70628e0f44db416d021c1992b8e8e3d78b814f
Zac Carman <57148819+ZacCarman@users.noreply.github.com>
2022-11-30 18:21:40 -0500

    Assignment 2 code

---
+++ assignment 2/Debug/Core/Src/lsm303agr.d
@@ -0,0 +1,6 @@
+Core/Src/lsm303agr.o: \
+ C:/Users/zacca/STM32CubeIDE/workspace_1.10.1/Demonstrations/Drivers/BSP/Components/lsm303agr/lsm303agr.c \
+ C:/Users/zacca/STM32CubeIDE/workspace_1.10.1/Demonstrations/Drivers/BSP/Components/lsm303agr/lsm303agr.h \
+ c:\users\zacca\stm32cubeide\workspace_1.10.1\demonstrations\drivers\bsp\components\common\accelero.h
+C:/Users/zacca/STM32CubeIDE/workspace_1.10.1/Demonstrations/Drivers/BSP/Components/lsm303agr/lsm303agr.h:
+c:\users\zacca\stm32cubeide\workspace_1.10.1\demonstrations\drivers\bsp\components\common\accelero.h:
---
+++ assignment 2/Debug/Core/Src/lsm303dlhc.d
@@ -0,0 +1,6 @@
+Core/Src/lsm303dlhc.o: \
+ C:/Users/zacca/STM32CubeIDE/workspace_1.10.1/Demonstrations/Drivers/BSP/Components/lsm303dlhc/lsm303dlhc.c \
+ C:/Users/zacca/STM32CubeIDE/workspace_1.10.1/Demonstrations/Drivers/BSP/Components/lsm303dlhc/lsm303dlhc.h \
+ c:\users\zacca\stm32cubeide\workspace_1.10.1\demonstrations\drivers\bsp\components\common\accelero.h
+C:/Users/zacca/STM32CubeIDE/workspace_1.10.1/Demonstrations/Drivers/BSP/Components/lsm303dlhc/lsm303dlhc.h:
+c:\users\zacca\stm32cubeide\workspace_1.10.1\demonstrations\drivers\bsp\components\common\accelero.h:
